0629f7a84ac1aea8c8a677aa98d31878064d264a
zakiafada32 <afadazaki32@gmail.com>
2024-12-06 10:25:44 +0700

    Add authentication module and account management; update migrations and routes

--- handle-errors/src/lib.rs
+++ handle-errors/src/lib.rs
@@ -1,3 +1,4 @@
+use argon2::Error as ArgonError;
 use reqwest::Error as ReqwestError;
 use reqwest_middleware::Error as MiddlewareReqwestError;
 use tracing::{Level, event, instrument};
@@ -12,7 +13,11 @@ use warp::{
 pub enum Error {
     ParseError(std::num::ParseIntError),
     MissingParameters,
-    DatabaseQueryError,
+    WrongPassword,
+    CannotDecryptToken,
+    Unauthorized,
+    ArgonLibraryError(ArgonError),
+    DatabaseQueryError(sqlx::Error),
     ReqwestAPIError(ReqwestError),
     MiddlewareReqwestAPIError(MiddlewareReqwestError),
     ClientError(APILayerError),
@@ -34,33 +39,81 @@ impl std::fmt::Display for APILayerError {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &*self {
-            Error::ParseError(err) => write!(f, "Cannot parse parameter: {}", err),
+            Error::ParseError(err) => {
+                write!(f, "Cannot parse parameter: {}", err)
+            }
             Error::MissingParameters => write!(f, "Missing parameter"),
-            Error::DatabaseQueryError => write!(f, "Cannot update, invalid data."),
-            Error::ReqwestAPIError(err) => write!(f, "External API error: {}", err),
-            Error::MiddlewareReqwestAPIError(err) => write!(f, "External API error: {}", err),
-            Error::ClientError(err) => write!(f, "External Client error: {}", err),
-            Error::ServerError(err) => write!(f, "External Server error: {}", err),
+            Error::WrongPassword => write!(f, "Wrong password"),
+            Error::CannotDecryptToken => write!(f, "Cannot decrypt error"),
+            Error::Unauthorized => write!(f, "No permission to change the underlying resource"),
+            Error::ArgonLibraryError(_) => {
+                write!(f, "Cannot verifiy password")
+            }
+            Error::DatabaseQueryError(_) => {
+                write!(f, "Cannot update, invalid data")
+            }
+            Error::ReqwestAPIError(err) => {
+                write!(f, "External API error: {}", err)
+            }
+            Error::MiddlewareReqwestAPIError(err) => {
+                write!(f, "External API error: {}", err)
+            }
+            Error::ClientError(err) => {
+                write!(f, "External Client error: {}", err)
+            }
+            Error::ServerError(err) => {
+                write!(f, "External Server error: {}", err)
+            }
         }
     }
 }
+
+const DUPLICATE_KEY: u32 = 23505;
 
 impl Reject for Error {}
 impl Reject for APILayerError {}
 
 #[instrument]
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(crate::Error::DatabaseQueryError) = r.find() {
+    if let Some(crate::Error::DatabaseQueryError(e)) = r.find() {
         event!(Level::ERROR, "Database query error");
-        Ok(warp::reply::with_status(
-            crate::Error::DatabaseQueryError.to_string(),
-            StatusCode::UNPROCESSABLE_ENTITY,
-        ))
+
+        match e {
+            sqlx::Error::Database(err) => {
+                if err.code().unwrap().parse::<u32>().unwrap() == DUPLICATE_KEY {
+                    Ok(warp::reply::with_status(
+                        "Account already exsists".to_string(),
+                        StatusCode::UNPROCESSABLE_ENTITY,
+                    ))
+                } else {
+                    Ok(warp::reply::with_status(
+                        "Cannot update data".to_string(),
+                        StatusCode::UNPROCESSABLE_ENTITY,
+                    ))
+                }
+            }
+            _ => Ok(warp::reply::with_status(
+                "Cannot update data".to_string(),
+                StatusCode::UNPROCESSABLE_ENTITY,
+            )),
+        }
     } else if let Some(crate::Error::ReqwestAPIError(e)) = r.find() {
         event!(Level::ERROR, "{}", e);
         Ok(warp::reply::with_status(
             "Internal Server Error".to_string(),
             StatusCode::INTERNAL_SERVER_ERROR,
+        ))
+    } else if let Some(crate::Error::Unauthorized) = r.find() {
+        event!(Level::ERROR, "Not matching account id");
+        Ok(warp::reply::with_status(
+            "No permission to change underlying resource".to_string(),
+            StatusCode::UNAUTHORIZED,
+        ))
+    } else if let Some(crate::Error::WrongPassword) = r.find() {
+        event!(Level::ERROR, "Entered wrong password");
+        Ok(warp::reply::with_status(
+            "Wrong E-Mail/Password combination".to_string(),
+            StatusCode::UNAUTHORIZED,
         ))
     } else if let Some(crate::Error::MiddlewareReqwestAPIError(e)) = r.find() {
         event!(Level::ERROR, "{}", e);
--- src/store.rs
+++ src/store.rs
@@ -1,9 +1,12 @@
-use sqlx::postgres::{PgPool, PgPoolOptions, PgRow};
-use sqlx::Row;
+use sqlx::{
+    postgres::{PgPool, PgPoolOptions, PgRow},
+    Row,
+};
 
 use handle_errors::Error;
 
 use crate::types::{
+    account::{Account, AccountId},
     answer::{Answer, AnswerId, NewAnswer},
     question::{NewQuestion, Question, QuestionId},
 };
@@ -36,7 +39,7 @@ impl Store {
     ) -> Result<Vec<Question>, Error> {
         match sqlx::query("SELECT * from questions LIMIT $1::int4 OFFSET $2")
             .bind(limit.map(|l| l as i32))
-            .bind(offset.map(|l| l as i32))
+            .bind(offset.map(|o| o as i32))
             .map(|row: PgRow| Question {
                 id: QuestionId(row.get("id")),
                 title: row.get("title"),
@@ -49,47 +52,72 @@ impl Store {
             Ok(questions) => Ok(questions),
             Err(e) => {
                 tracing::event!(tracing::Level::ERROR, "{:?}", e);
-                Err(Error::DatabaseQueryError)
+                Err(Error::DatabaseQueryError(e))
             }
         }
     }
 
-    pub async fn add_question(self, new_question: NewQuestion) -> Result<Question, Error> {
-        match sqlx::query(
-            "INSERT INTO questions (title, content, tags) 
-                    VALUES ($1, $2, $3) 
-                    RETURNING id, title, content, tags",
-        )
-        .bind(new_question.title)
-        .bind(new_question.content)
-        .bind(new_question.tags)
-        .map(|row: PgRow| Question {
-            id: QuestionId(row.get("id")),
-            title: row.get("title"),
-            content: row.get("content"),
-            tags: row.get("tags"),
-        })
-        .fetch_one(&self.connection)
-        .await
+    pub async fn is_question_owner(
+        &self,
+        question_id: i32,
+        account_id: &AccountId,
+    ) -> Result<bool, Error> {
+        match sqlx::query("SELECT * from questions where id = $1 and account_id = $2")
+            .bind(question_id)
+            .bind(account_id.0)
+            .fetch_optional(&self.connection)
+            .await
         {
-            Ok(question) => Ok(question),
+            Ok(question) => Ok(question.is_some()),
             Err(e) => {
                 tracing::event!(tracing::Level::ERROR, "{:?}", e);
-                Err(Error::DatabaseQueryError)
+                Err(Error::DatabaseQueryError(e))
             }
         }
     }
 
-    pub async fn update_question(self, question: Question, id: i32) -> Result<Question, Error> {
+    pub async fn add_question(
+        self,
+        new_question: NewQuestion,
+        account_id: AccountId,
+    ) -> Result<Question, Error> {
+        match sqlx::query("INSERT INTO questions (title, content, tags, account_id) VALUES ($1, $2, $3, $4) RETURNING id, title, content, tags")
+            .bind(new_question.title)
+            .bind(new_question.content)
+            .bind(new_question.tags)
+            .bind(account_id.0)
+            .map(|row: PgRow| Question {
+			    id: QuestionId(row.get("id")),
+                title: row.get("title"),
+			    content: row.get("content"),
+                tags: row.get("tags"),
+		    })
+            .fetch_one(&self.connection)
+            .await {
+                Ok(question) => Ok(question),
+                Err(error) => {
+                    tracing::event!(tracing::Level::ERROR, "{:?}", error);
+                    Err(Error::DatabaseQueryError(error))
+                },
+            }
+    }
+
+    pub async fn update_question(
+        self,
+        question: Question,
+        id: i32,
+        account_id: AccountId,
+    ) -> Result<Question, Error> {
         match sqlx::query(
             "UPDATE questions SET title = $1, content = $2, tags = $3
-                    WHERE id = $4
+                    WHERE id = $4 AND account_id = $5
                     RETURNING id, title, content, tags",
         )
         .bind(question.title)
         .bind(question.content)
         .bind(question.tags)
         .bind(id)
+        .bind(account_id.0)
         .map(|row: PgRow| Question {
             id: QuestionId(row.get("id")),
             title: row.get("title"),
@@ -100,35 +128,39 @@ impl Store {
         .await
         {
             Ok(question) => Ok(question),
-            Err(e) => {
-                tracing::event!(tracing::Level::ERROR, "{:?}", e);
-                Err(Error::DatabaseQueryError)
+            Err(error) => {
+                tracing::event!(tracing::Level::ERROR, "{:?}", error);
+                Err(Error::DatabaseQueryError(error))
             }
         }
     }
 
-    pub async fn delete_question(self, id: i32) -> Result<bool, Error> {
-        match sqlx::query("DELETE FROM questions WHERE id = $1")
+    pub async fn delete_question(self, id: i32, account_id: AccountId) -> Result<bool, Error> {
+        match sqlx::query("DELETE FROM questions WHERE id = $1 AND account_id = $2")
             .bind(id)
+            .bind(account_id.0)
             .execute(&self.connection)
             .await
         {
             Ok(_) => Ok(true),
             Err(e) => {
                 tracing::event!(tracing::Level::ERROR, "{:?}", e);
-                Err(Error::DatabaseQueryError)
+                Err(Error::DatabaseQueryError(e))
             }
         }
     }
 
-    pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<Answer, Error> {
+    pub async fn add_answer(
+        self,
+        new_answer: NewAnswer,
+        account_id: AccountId,
+    ) -> Result<Answer, Error> {
         match sqlx::query(
-            "INSERT INTO answers (content, question_id) 
-                    VALUES ($1, $2)
-                    RETURNING id, content, question_id",
+            "INSERT INTO answers (content, question_id, account_id) VALUES ($1, $2, $3) RETURNING id, content, question_id",
         )
         .bind(new_answer.content)
         .bind(new_answer.question_id.0)
+        .bind(account_id.0)
         .map(|row: PgRow| Answer {
             id: AnswerId(row.get("id")),
             content: row.get("content"),
@@ -138,9 +170,65 @@ impl Store {
         .await
         {
             Ok(answer) => Ok(answer),
-            Err(e) => {
-                tracing::event!(tracing::Level::ERROR, "{:?}", e);
-                Err(Error::DatabaseQueryError)
+            Err(error) => {
+                tracing::event!(
+                    tracing::Level::ERROR,
+                    code = error
+                        .as_database_error()
+                        .unwrap()
+                        .code()
+                        .unwrap()
+                        .parse::<i32>()
+                        .unwrap(),
+                    db_message = error.as_database_error().unwrap().message(),
+                    constraint = error.as_database_error().unwrap().constraint().unwrap()
+                );
+                Err(Error::DatabaseQueryError(error))
+            }
+        }
+    }
+
+    pub async fn add_account(self, account: Account) -> Result<bool, Error> {
+        match sqlx::query("INSERT INTO accounts (email, password) VALUES ($1, $2)")
+            .bind(account.email)
+            .bind(account.password)
+            .execute(&self.connection)
+            .await
+        {
+            Ok(_) => Ok(true),
+            Err(error) => {
+                tracing::event!(
+                    tracing::Level::ERROR,
+                    code = error
+                        .as_database_error()
+                        .unwrap()
+                        .code()
+                        .unwrap()
+                        .parse::<i32>()
+                        .unwrap(),
+                    db_message = error.as_database_error().unwrap().message(),
+                    constraint = error.as_database_error().unwrap().constraint().unwrap()
+                );
+                Err(Error::DatabaseQueryError(error))
+            }
+        }
+    }
+
+    pub async fn get_account(self, email: String) -> Result<Account, Error> {
+        match sqlx::query("SELECT * from accounts where email = $1")
+            .bind(email)
+            .map(|row: PgRow| Account {
+                id: Some(AccountId(row.get("id"))),
+                email: row.get("email"),
+                password: row.get("password"),
+            })
+            .fetch_one(&self.connection)
+            .await
+        {
+            Ok(account) => Ok(account),
+            Err(error) => {
+                tracing::event!(tracing::Level::ERROR, "{:?}", error);
+                Err(Error::DatabaseQueryError(error))
             }
         }
     }
